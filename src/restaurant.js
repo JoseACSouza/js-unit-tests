54c22b29b2c97f1e034cb6d99eaf91ba6da439e6
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-01-23 22:02:45 -0300

    começando a implementar o requisito 10

--- src/restaurant.js
+++ src/restaurant.js
@@ -41,7 +41,13 @@
 // 4: Crie uma função `createMenu()` que, recebendo um objeto como parâmetro, retorna esse objeto no seguinte formato: 
 //  { fetchMenu: () => objetoPassadoPorParametro }.
 
-const createMenu = () => {};
+const createMenu = (food, drink) => (
+{
+  fetchMenu: () => (
+  food,
+  drink,)
+})   
+;
 
 // Faça o item 5 no arquivo tests/restaurant.spec.js
 
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -36,9 +36,8 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // 1: Verifique se função `createMenu()` retorna um objeto que possui a chave `fetchMenu`, a qual tem como valor uma função.
-
+    expect(typeof createMenu(fetchMenu)).toBe('function');
     // 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
     // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
 
